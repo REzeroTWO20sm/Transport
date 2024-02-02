d5d8980e27b8db0da04698b4d48d4fae4630441c
Robert <robert2005n@gmail.com>
2024-02-02 07:14:18 +0300

    gaba 5

--- Transport.cpp
+++ Transport.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
+#include <string>
 
 //g++ Transport.cpp -o Transport --std=c++14 -I/usr/local/include -I/usr/include/nlohmann -lcurl
 
@@ -8,242 +9,139 @@ using namespace std;
 using json = nlohmann::json;
 
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    ((string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
-string take_closer_station(string address,string station_types){
-    CURL *curl;
-    CURLcode res;
-    string readBuffer;
-    stringstream lat_ss;
-    stringstream lon_ss;
-    float lat_f;
-    float lon_f;
-    string lat;
-    string lon;
-    string station;
-    string url;
-    curl = curl_easy_init();
-    if(curl){
-        //"https://catalog.api.2gis.com/3.0/items/geocode?q=Москва, Садовническая, 25&fields=items.point&key=0038d200d-62ed-4147-8a11-b996a7e2c7a7
-        url = "https://catalog.api.2gis.com/3.0/items/geocode?q=" + address + "&fields=items.point&key=0038d200-62ed-4147-8a11-b996a7e2c7a7";
-        /* string url = "https://geocode-maps.yandex.ru/1.x/?apikey=dbcd5d6e-f034-4c9e-a84d-11c201dbd4d4&lang=ru_RU&geocode="+ city +"&format=json"; определение station города*/
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK){
-            json response = json::parse(readBuffer);
-            lat_f = response["result"]["items"][0]["point"]["lat"];
-            lon_f = response["result"]["items"][0]["point"]["lon"];
-            lat_ss << lat_f;
-            lon_ss << lon_f;
-            lat  = lat_ss.str();
-            lon = lon_ss.str();
-            /*pos = pos.replace(pos.find(" "),1,"&lat=");*/                                                                                                                        //37.617698&lan=55.755864
-            //https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&format=json&lng=&37.617698lat=55.755864&distance=50&lang=ru_RU"
-            
-        }
-        else {
-            std::cerr << "Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        readBuffer.clear();
-        string url_1 = "https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&format=json&station_types="+station_types+"&lng=" + lon + "&lat=" + lat + "&distance=50&lang=ru_RU";
-        curl_easy_setopt(curl, CURLOPT_URL, url_1.c_str());
-        res = curl_easy_perform(curl);
-        
-        if(res == CURLE_OK){
-            json response = json::parse(readBuffer);
-            int i=0;
-            while(!response["stations"][i].is_null()){
-                station += response["stations"][i]["code"];
-                station += " ";
-                station += response["stations"][i]["title"];
-                station += " , ";
-                i+=1;
-            }
-        }
-        else{
-            std::cerr << endl<<"Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
-    return station;
-}
-string take_station(string address, string station_types){
-    CURL *curl;
-    CURLcode res;
-    string readBuffer;
-    stringstream lat_ss;
-    stringstream lon_ss;
-    float lat_f;
-    float lon_f;
-    string lat;
-    string lon;
-    string station;
-    string url;
-    curl = curl_easy_init();
-    if(curl){
-        //"https://catalog.api.2gis.com/3.0/items/geocode?q=Москва, Садовническая, 25&fields=items.point&key=0038d200d-62ed-4147-8a11-b996a7e2c7a7
-        url = "https://catalog.api.2gis.com/3.0/items/geocode?q=" + address + "&fields=items.point&key=0038d200-62ed-4147-8a11-b996a7e2c7a7";
-        /* string url = "https://geocode-maps.yandex.ru/1.x/?apikey=dbcd5d6e-f034-4c9e-a84d-11c201dbd4d4&lang=ru_RU&geocode="+ city +"&format=json"; определение station города*/
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK){
-            json response = json::parse(readBuffer);
-            lat_f = response["result"]["items"][0]["point"]["lat"];
-            lon_f = response["result"]["items"][0]["point"]["lon"];
-            lat_ss << lat_f;
-            lon_ss << lon_f;
-            lat  = lat_ss.str();
-            lon = lon_ss.str();
-            /*pos = pos.replace(pos.find(" "),1,"&lat=");*/                                                                                                                        //37.617698&lan=55.755864
-            
-        }
-        else {
-            std::cerr << "Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        readBuffer.clear();
-        string url_1 = "https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&format=json&station_types=" + station_types + "&lng=" + lon + "&lat=" + lat + "&distance=50&lang=ru_RU";
-        
-        curl_easy_setopt(curl, CURLOPT_URL, url_1.c_str());
-        res = curl_easy_perform(curl);
-        
-        if(res == CURLE_OK){
-            json response = json::parse(readBuffer);
-            station = response["stations"][0]["code"];
-            
-        }
-        else{
-            std::cerr << endl<<"Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
-    return station;
-}
-json Timing_transport(string cityA,string cityB,string date){
-    CURL *curl;
-    CURLcode res;
-    string readBuffer;
-    json response;
 
-    curl = curl_easy_init();
-    if(curl){
-        
-        string url = "https://api.rasp.yandex.net/v3.0/search/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&date=" + date + "&from=" + cityA + "&to=" + cityB ; /*определение station города*/
-        cout << url ;
+template<typename T>
+string float_to_str(T a_f){
+    stringstream a_ss;
+    string a_s;
+    a_ss << a_f;
+    a_s  = a_ss.str();
+    return a_s;
+}
+
+string cUrl(const string& url) {
+    CURL* curl = curl_easy_init();
+    if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        string response;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK){
-            response = json::parse(readBuffer);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        CURLcode res = curl_easy_perform(curl);
+        if (res == CURLE_OK) {
+            curl_easy_cleanup(curl);
             return response;
+        } else {
+            std::cout << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
         }
-        else {
-            std::cerr << "Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        curl_easy_cleanup(curl);
     }
-    curl_global_cleanup();
-    return response;
+    return "";
+}
+
+
+void take_closer_station(string address,string station_types){
+    string station;
+    string url;
+
+    url = "https://catalog.api.2gis.com/3.0/items/geocode?q=" + address + "&fields=items.point&key=0038d200-62ed-4147-8a11-b996a7e2c7a7";
+    json response = json::parse(cUrl(url));
+
+    float lat_f = response["result"]["items"][0]["point"]["lat"];
+    float lon_f = response["result"]["items"][0]["point"]["lon"];
+
+    string url_1 = "https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&format=json&station_types="+station_types+"&lng=" + float_to_str(lon_f) + "&lat=" + float_to_str(lat_f) + "&distance=50&lang=ru_RU";  
+    json response_1 = json::parse(cUrl(url_1));
+    int i=0;
+    while(!response_1["stations"][i].is_null()){
+        station += response_1["stations"][i]["code"];
+        station += " ";
+        station += response_1["stations"][i]["title"];
+        station += " , ";
+        i+=1;
+    }
+    cout << endl << station << endl;
+}
+
+
+string take_station(string address, string station_types){
+    string station;
+    string url;
+    url = "https://catalog.api.2gis.com/3.0/items/geocode?q=" + address + "&fields=items.point&key=0038d200-62ed-4147-8a11-b996a7e2c7a7";
+    json response = json::parse(cUrl(url));
+    float lat_f = response["result"]["items"][0]["point"]["lat"];
+    float lon_f = response["result"]["items"][0]["point"]["lon"];
+    string url_1 = "https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&format=json&station_types=" + station_types + "&lng=" + float_to_str(lon_f) + "&lat=" + float_to_str(lat_f) + "&distance=50&lang=ru_RU";
+    json response_1 = json::parse(cUrl(url_1));
+    station = response_1["stations"][0]["code"];
+    return station;
+}
+
+void Timing_transport(string cityA,string cityB,string date){
+    string url = "https://api.rasp.yandex.net/v3.0/search/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&date=" + date + "&from=" + cityA + "&to=" + cityB ;
+    json response = json::parse(cUrl(url));
+    
+    int i=0;
+    while(!response["segments"][i].is_null()){
+        std::cout << endl << response["segments"][i]["thread"]["title"] << endl 
+        << response["segments"][i]["from"]["title"] << " - " << response["segments"][i]["to"]["title"] << endl
+        << response["segments"][i]["start_date"] << endl << response["segments"][i]["arrival"] << endl;
+        i+=1;
+    }
 }
 
 void transport_inf(string cityA,string cityB,string date){
-    CURL *curl;
-    CURLcode res;
-    string readBuffer;
-    json response;
-    json response_1;
-    int transport_code_i;
-    stringstream transport_code_ss;
-    string transport_code;
-    curl = curl_easy_init();
-
-    if(curl){
-        
-        string url = "https://api.rasp.yandex.net/v3.0/search/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&date=" + date + "&from=" + cityA + "&to=" + cityB ; /*определение station города*/
-        cout << url << endl;
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK){
-            int i=0;
-            response = json::parse(readBuffer);
-            readBuffer.clear();
-            while(!response["segments"][i].is_null()){
-                transport_code_ss.str("");
-                transport_code_i = response["segments"][i]["thread"]["carrier"]["code"];
-                transport_code_ss << transport_code_i;
-                transport_code = transport_code_ss.str();
-                cout << endl << transport_code << endl;
-                url = "https://api.rasp.yandex.net/v3.0/carrier/?format=json&apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&lang=ru_RU&code=" + transport_code ;
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-                res = curl_easy_perform(curl);
-                if(res == CURLE_OK){
-                    response_1 = json::parse(readBuffer);
-                    cout << endl << response_1<< endl;
-                }
-                else {
-                    std::cerr << "Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-                }
-                i += 1;
-                readBuffer.clear();
-            }
-        }
-        else {
-            std::cerr << "Ошибка при выполнении запроса: " << curl_easy_strerror(res) << std::endl;
-        }
-        curl_easy_cleanup(curl);
+    string url = "https://api.rasp.yandex.net/v3.0/search/?apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&date=" + date + "&from=" + cityA + "&to=" + cityB ; /*определение station города*/
+    int i=0;
+    json response = json::parse(cUrl(url));
+    while(!response["segments"][i].is_null()){
+        int transport_code_i = response["segments"][i]["thread"]["carrier"]["code"];
+        cout << endl << float_to_str(transport_code_i) << endl;
+        url = "https://api.rasp.yandex.net/v3.0/carrier/?format=json&apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&lang=ru_RU&code=" + float_to_str(transport_code_i);
+        json response_1 = json::parse(cUrl(url));
+        cout << endl << response_1<< endl;
+        i += 1;
     }
-
-
-    // "https://api.rasp.yandex.net/v3.0/carrier/?format=json&apikey=a28bf47e-da92-4a54-a246-f0e7a7ed4818&lang=ru_RU&code=" + transport_code + "&system=iata"
-
-
-    curl_global_cleanup();
 }
 
 int main(){
-    string adress_A; // 
-    string adress_B; //
-    string station_types; //
-    string station;
-    string station_1;
-    string closer_station;
-    string date; // 
-    json Timings; 
+    string adress_A_s;
+    string adress_B_s;
+    string date,adress_A,adress_B,station_type;
+    cin>>date>>adress_A>>adress_B>>station_type;//YYYY-MM-DD,Москва,Казань,airport
 
-    adress_A = "москва";
-    adress_B = "Казань";
+    adress_A_s = take_station(adress_A,station_type);
+    adress_B_s = take_station(adress_B,station_type);
 
-    date = "2024-01-25";
-
-    station_types = "airport";
-
-    station = take_station(adress_A,station_types);
-    cout << endl<<station<<endl;
-    station_1 = take_station(adress_B,station_types);
-    Timing_transport(station,station_1,date);
-    Timings = Timing_transport(station,station_1,date);
-
-    for (int i=0;i<=10;i++) {
-        if(Timings.is_null() /*and Timings["segments"][i+1]["thread"]["title"]!="null"*/){
-            break;
+    int cifra;
+    while(cifra != 4){
+        cout << endl << "Меню:" << endl;
+        cout << "1. Просмотр рейсов из A в B по зад. дате" << endl;
+        cout << "2. Ближайшая станции" << endl;
+        cout << "3. Информация о перевозчике" << endl;
+        cout << "4. Выход" << endl;
+        cin >> cifra;
+        switch (cifra) {
+            case 1:
+                Timing_transport(adress_A_s,adress_B_s,date);
+                break;
+            case 2:
+                take_closer_station(adress_A,station_type);
+                break;
+            case 3:
+                transport_inf(adress_A_s,adress_B_s,date);
+                break;
+            case 4:
+                cout << "Выход из программы." << endl;
+                break;
+            default:
+                cout << "Выбрано неверное действие. Попробуйте снова." << endl;
+                break;
         }
-        else{
-            cout << endl << Timings["segments"][i]["thread"]["title"] << endl 
-            << Timings["segments"][i]["from"]["title"] << " - " << Timings["segments"][i]["to"]["title"] << endl
-            << Timings["segments"][i]["start_date"] << endl << Timings["segments"][i]["arrival"] << endl;
-        }
+
+        cout << endl;
     }
-    closer_station = take_closer_station(adress_A,station_types);
-    cout << endl << closer_station << endl << endl << endl;
-
-    transport_inf(station,station_1,date);
 }
