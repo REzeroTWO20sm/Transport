1f96036c2d2c6bc7ea67728c81d94d70a61dcbbe
Robert <robert2005n@gmail.com>
2024-02-07 15:31:01 +0300

    gaba 10

---
+++ build/CMakeFiles/Transport.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transport.dir/Transport.cpp.o"
+  "CMakeFiles/Transport.dir/Transport.cpp.o.d"
+  "Transport"
+  "Transport.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Transport.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Transport.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Transport.
+# This may be replaced when dependencies are built.
